4bd777473b4c22a9486b1b3b734f2dcb9c2a7bd0
root <testlolabtest@gmail.com>
2023-10-08 11:45:12 +0000

    Add the list to be returned and the loops with the correct logic to return pascals values

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,4 +6,12 @@ def pascal_triangle(n):
     """"Pascals triangle"""
     if (n <= 0):
         return []
-    pass
+    new_list = [[1]]
+    for i in range(1, n):
+        sec_list = [1]
+        for j in range(1, i):
+            num = new_list[i - 1][j - 1] + new_list[i - 1][j]
+            sec_list.append(num)
+        sec_list.append(1)
+        new_list.append(sec_list)
+    return new_list
