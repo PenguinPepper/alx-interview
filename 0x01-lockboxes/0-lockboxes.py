12d3403ceb04a027d2e960a1d9282ed6b4b4afa2
root <testlolabtest@gmail.com>
2023-10-08 13:50:35 +0000

    Add solution that return false only

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,4 +3,13 @@
 
 
 def canUnlockAll(boxes):
-    pass
+    """Sees if boxes can be unlocked"""
+    num_boxes = len(boxes)
+    for i in range(num_boxes):
+        for j in range(num_boxes):
+            key = boxes[i][j]
+            if (key == i):
+                return key
+            else:
+                return key
+
