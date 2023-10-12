3889dd6c6ae579255909c5ef76e99ff331000ddb
root <testlolabtest@gmail.com>
2023-10-12 09:46:52 +0000

    Fix logic of function to check for the biggest factor of n as it is the number of operations needed to get n number of charcters in files

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,19 +3,14 @@
 
 
 def minOperations(n):
-    """Returns an int"""
-    if( n <= 7):
-        return n
-    elif(n % 3 == 0):
-        return n // 3 + 3
-    elif(n % 2 == 0):
-        return n // 2 + 1
-    else:
+    """Find the minimum number of operations needed
+    to get to n number of 'H' characters in a file
+    
+    n: number of characters in the file
+    """
+    if (n <= 1):
         return 0
-"""
-In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
-
-Prototype: def minOperations(n)
-Returns an integer
-If n is impossible to achieve, return 0
-"""
+    for i in range(2, n + 1):
+        if (n % i == 0):
+            return i + minOperations(n // i)
+    return n
