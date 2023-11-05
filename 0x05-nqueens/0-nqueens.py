96637696a72f902080075195d0c0bfeb08628208
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-05 07:47:24 +0000

    Add nqueens script that handles edge cases first

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module contains nqueens script that solves the N queens problem.
+where N must be an integer greater or equal to 4
+If N is not an integer, print N must be a number,
+followed by a new line, and exit with the status 1
+If N is smaller than 4, print N must be at least 4,
+followed by a new line, and exit with the status 1 The
+program should print every possible solution to the problem
+One solution per line
+"""
+import sys
+
+
+if (len(sys.argv) <= 1):
+    print("Usage: nqueens N")
+    sys.exit(1)
+else:
+    try:
+        num = int(sys.argv[1])
+    except ValueError as error:
+        print("N must be a number")
+        sys.exit(1)
+    else:
+        if (num < 4):
+            print("N must be at least 4")
+            sys.exit(1)
+        else:
+            pass
