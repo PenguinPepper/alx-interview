dcf135c47cd34a8f5914a2ad5afff236d49a4ec9
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-12 09:51:01 -0800

    Catch errors if there are any

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -10,13 +10,13 @@ const request = require('request');
 const movieId = process.argv[2];
 
 request('https://swapi-api.alx-tools.com/api/films/' + movieId, function (error, res, body) {
+  if (error) throw error;
   const response = JSON.parse(body);
   for (const character of response.characters) {
     request(character, function (err, ress, bod) {
       const characterBody = JSON.parse(bod);
       console.log(characterBody.name);
-      console.error(err);
+      if (err) throw err;
     });
   }
-  console.error(error);
 });
